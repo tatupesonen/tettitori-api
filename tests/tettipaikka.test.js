dd2db1dbce9d53f35532df864974207ece0fefb7
Tatu Pesonen <tatu@farmatools.fi>
2020-02-18 14:48:14 +0200

    Update test suite for tettipaikka to wait for mongodb sync

--- tests/tettipaikka.test.js
+++ tests/tettipaikka.test.js
@@ -11,12 +11,21 @@ beforeAll(async () => await dbHandler.connect())
 
 afterAll(async () => await dbHandler.closeDatabase())
 
-/* Plain object for testing, only required fields */
+/* Plain objects for testing, only required fields */
 const tettipaikkaComplete = {
     title: 'Tiiriön ABC',
     tehtavat: 'Siivousta',
     yhteydenottotapa: 'Tekstiviestitse',
 }
+
+const tettipaikkaComplete2 = {
+    title: 'Markon siivousliike',
+    tehtavat: 'Imurointia',
+    yhteydenottotapa: 'Sähköpostitse',
+}
+
+//We will need this in our tests
+let singleId = ''
 
 /* Tettipaikka test suite */
 describe('tettipaikka ', () => {
@@ -26,13 +35,41 @@ describe('tettipaikka ', () => {
         ).not.toThrow()
     })
 
-    it('database returns all objects', async () => {
-        expect(typeof (await tettipaikkaService.getAllPaikka())).toBe('object')
+    it('second one can be created correctly', async () => {
+        expect(
+            async () => await tettipaikkaService.create(tettipaikkaComplete2)
+        ).not.toThrow()
     })
 
-    it('database returns single object', async () => {
-        expect(
-            typeof (await tettipaikkaService.getSinglePaikka('Tiir')._id)
-        ).not.toBeNull()
+    it('wait for db sync', async () => {
+        await new Promise(r => setTimeout(r, 100))
+    })
+
+    it('database returns single object with full name match', async () => {
+        const result = await tettipaikkaService.getSinglePaikka('Tiiriön ABC')
+        expect(typeof result._id).not.toBeNull()
+    })
+
+    it('database returns single object with partial match', async () => {
+        const result = await tettipaikkaService.getSinglePaikka('Tiir')
+        singleId = result._id
+        expect(typeof result._id).not.toBeNull()
+    })
+
+    it('database returns all objects', async () => {
+        let result = await tettipaikkaService.getAllPaikka()
+        let log = ''
+        result.forEach(r => {
+            log = log + `${r.title} \n`
+        })
+        console.log(
+            'MongoDB collection status \n---------------------' + '\n' + log
+        )
+        expect(typeof result).toBe('object')
+    })
+
+    it('can delete the single object', async () => {
+        let result = await tettipaikkaService.delete(singleId)
+        expect(result.deletedCount).toBe(1)
     })
 })
